6c1d3e1e96f18f1521d9834e8db79e761a235d62
pandeydeep1318 <59316266+pandeydeep1318@users.noreply.github.com>
2020-03-30 11:54:06 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,5 +1,4 @@
-/*
- *  Created by IntelliJ IDEA.
+ *Created by IntelliJ IDEA.
  *  User: Vaibhav
  *  Date: 23-Mar-20
  *  Time: 7:17 PM
@@ -7,5 +6,20 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        bst.insert(85);
+        bst.insert(25);
+        bst.insert(45);
+        bst.insert(15);
+        bst.insert(17);
+        bst.insert(15);
+        bst.insert(10);
+        bst.insert(1);
+        bst.diaplay(bst.getRoot(), bst.getMlevel());
+        bst.d2();
+    }
 }
